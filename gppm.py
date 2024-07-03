80bdbf4baa3b11940646f11f677bce0da28bc69b
sulpher <sulpher@gmx.net>
2024-07-03 17:11:48 +0200

    Fixed json output

--- gppm.py
+++ gppm.py
@@ -11,19 +11,17 @@ def cli():
 @cli.command()
 def reload_llamacpp_configs():
     response = requests.get(f"{BASE_URL}/reload_llamacpp_configs")
-    print(response.json())
+    print(json.dumps(response.json()))
 
 @cli.command()
 def get_llamacpp_instances():
     response = requests.get(f"{BASE_URL}/get_llamacpp_instances")
-    #print(response.text)
-    print(response.json())
+    print(json.dumps(response.json()))
 
 @cli.command()
 def get_llamacpp_configs():
     response = requests.get(f"{BASE_URL}/get_llamacpp_configs")
-    #print(response.text)
-    print(response.json())
+    print(json.dumps(response.json()))
 
 if __name__ == "__main__":
     cli()
