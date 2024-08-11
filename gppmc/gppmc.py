24b3871530edc13abcbd5813fd804be27a376ec4
sulpher <sulpher@gmx.net>
2024-08-11 12:37:30 +0200

    added format option

--- gppmc/gppmc.py
+++ gppmc/gppmc.py
@@ -3,6 +3,7 @@ import json
 import click
 import click_completion
 from halo import Halo
+import yaml
 
 click_completion.init()
 
@@ -19,11 +20,39 @@ def get_group():
     pass
 
 @get_group.command('instances')
-def get_instances():
+@click.option('--format', default='text', type=click.Choice(['json', 'text']), help='Print output in JSON or text format.')
+def get_instances(format):
     """Get all LlamaCpp instances."""
-    with Halo(text='Loading instances', spinner='dots'):
+    if format == 'text':
+        with Halo(text='Loading instances', spinner='dots'):
+            response = requests.get(f"{BASE_URL}/get_llamacpp_instances")
+    else:
         response = requests.get(f"{BASE_URL}/get_llamacpp_instances")
-    print(json.dumps(response.json(), indent=4))
+
+    if format == 'json':
+        #print(json.dumps(response.json(), indent=4))
+        print(response.json())
+    else:
+        instances = response.json()["llamacpp_instances"]
+        for instance in instances:
+            print(instance)
+
+
+@gppmc.command('apply')
+@click.argument('file', type=click.File('rb'))
+@click.option('--format', default='text', type=click.Choice(['json', 'text']), help='Print output in JSON or text format.')
+def apply_configs(file, format):
+    """Apply LlamaCpp configurations from a YAML file."""
+    data = yaml.safe_load(file)
+
+    if format == 'text':
+        with Halo(text='Applying configurations', spinner='dots'):
+            response = requests.post(f"{BASE_URL}/apply_llamacpp_configs", json=data)
+        #print(response.text)
+    else:
+        response = requests.post(f"{BASE_URL}/apply_llamacpp_configs", json=data)
+        print(response.json())
+
 
 @get_group.command('configs')
 def get_configs():
@@ -37,7 +66,7 @@ def reload_configs():
     """Reload LlamaCpp configurations."""
     with Halo(text='Reloading configurations', spinner='dots'):
         response = requests.get(f"{BASE_URL}/reload_llamacpp_configs")
-    #print(json.dumps(response.json(), indent=4))
+    print(json.dumps(response.json(), indent=4))
 
 if __name__ == "__main__":
     gppmc(auto_envvar_prefix='GPPMC')
