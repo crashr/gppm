21be304ce9f066058752f9b6f0c519d9a634f4c4
sulpher <sulpher@gmx.net>
2024-08-31 23:47:28 +0200

    improved sync script

--- tools/sync_to_remote.sh
+++ tools/sync_to_remote.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-SRC="./"
-DEST="pc06:~/gppm"
+# Description:
+# This script synchronizes a source directory to a remote destination using rsync.
+# It uses inotifywait to monitor the source directory for changes and then synchronizes the changes to the destination.
+# The source and destination directories are specified as command line arguments.
+
+# Check if source and destination are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <source_directory> <destination_directory>"
+    exit 1
+fi
+
+SRC="$1"
+DEST="$2"
 #LOGFILE="/var/log/rsync.log"
 
 inotifywait -m -r -e modify,create,delete,move "$SRC" --format '%w%f' |
